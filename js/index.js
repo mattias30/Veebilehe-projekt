e42ab94a3ad554545e73dd0f4385a2b737cc9c34
Jesper Korsen <Jesper.Korsen@gmail.com>
2024-12-01 16:15:44 +0200

    uuendatud baarid.yml ja nats javascripti

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
+
 var kaart = L.map('kaart').setView([58.37939333946564, 26.723323522250354],17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(kaart);
+
+// fetch('../data/baarid.json')
+//     .then((response) => response.json())
+//     .then((json) => {var data = json});
+
+
+for (const nimi of Object.keys(data.baarid)){
+    console.log(data.baarid[nimi]);
+};
