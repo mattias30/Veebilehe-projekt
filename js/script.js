38955dc751f799e1d3b42a0943517ea541009a58
Mattias Rahnu <rahnumat@ut.ee>
2024-12-02 21:15:14 +0200

    uuendus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function showInfo(drink) {
-  const infoDiv = document.querySelector('.info');
+/* function showInfo(drink) {
+  const infoDiv = document.querySelector('.info'); */
 
-  // Image and text maps for different drink types
-  const imageMap = {
+  // Vastav pilt vastava joogi jaoks
+/*   const imageMap = {
       "vein": "pildid_lehel_2/vein.jpg",
       "õlu": "pildid_lehel_2/olu.jpg",
       "džinn": "pildid_lehel_2/dzinn.jpg",
@@ -12,10 +12,11 @@ function showInfo(drink) {
       "tekiila": "pildid_lehel_2/tekiila.jpg",
       "brändi": "pildid_lehel_2/brandi.jpg",
       "liköörid": "pildid_lehel_2/likoorid.jpg",
-      "kokteilid": "pildid_lehel_2/kokteilid.jpg"
-  };
-
-  const textMap = {
+      "kokteilid": "pildid_lehel_2/kokteilid.jpg" */
+/*   };
+ */
+  //Vastav info
+/*   const textMap = {
       "vein": "Vein on alkohoolne jook, mis on saadud puuviljade fermenteerimise teel. Euroopa Liidus on lubatud veiniks nimetada vaid viinamarjadest kääritatud jooki. Veini on valmistatud üle 8000 aasta, sellega hakati esimest korda tegelema ühes kiviaegses asulas, mis asub tänapäeva Gruusias. Maitse järgi liigitatakse veinid kuivadeks, poolkuivadeks, poolmagusateks ja magusateks veinideks. Värvi järgi jaotuvad veinid punasteks, roosadeks ja valgeteks. Liigitatakse ka marjasordi järgi, suhkrusisalduse põhjal või kasvukoha pinnase kvaliteedi alusel. Lisaks kasutatakse veini valmistamisel mitmesuguseid, tihti piirkondlikest iseärasustest tingitud spetsiifilisi tehnoloogiaid, mille tulemuseks on näiteks portveinid, vahuveinid, šerrid, burgunderid jt.",
       "õlu": "Õlu on kääritamise teel teraviljast või muust tärkliserikkast toorainest valmistatud alkohoolne jook. Õlu on kõige populaarsem ja tõenäoliselt kõige vanem alkohoolne jook, olles vee ja tee järel kõige levinum jook üldse.Arheoloogid oletavad, et õllel oli oluline roll tsivilisatsioonide kujunemises. Varaseimad märgid õlle keemilise koostise kohta pärinevad ajavahemikust 3500-3100 eKr Godin Tepe asulakohast Z̄āgrose mäestikus Lääne-Iraanis. Õlle valmistamiseks kasvatatakse viljast linnased, millest valmistatakse meski, mis segatakse veega ja filtreeritakse ning saadakse virre. Virret keedetakse tavaliselt koos humalatega, mis toimivad konservandina ning annavad õllele iseloomuliku mõrkja maitse. Seejärel virre jahutatakse ja setitatakse ning sellele lisatakse kääritamiseks pärm.",
       "džinn": " Džinn on destilleeritud alkohoolne jook, millele on iseloomulik maitsestamine kadakamarjadega. Laukapuu džinni (inglise keeles sloe gin) valmistamisel kasutatakse maitse andmiseks laukaploome. Algselt hakati džinni valmistama Hollandis. Jook oli mõeldud ravimiks, millele maitse meeldivamaks muutmiseks lisati kadakamarju. Seetõttu sai uus alkohoolne jook nime genever (tuletatud prantsuskeelsest sõnast genièvre, mis tähendab kadakamarja). Inglismaal tutvustas jooki Oranje Willelm 17. sajandil ning inglased lühendasid joogi nimetuse gin'iks. Tänapäeval valmistatakse džinni eri viisidel, väga paljudest taimedest, on loodud palju stiile ja tootemarke.  Lisaks kadakamarjale annavad džinnile maitse ka maitsetaimed, ürdid, õistaimed või puuviljad ja tihtipeale nende kombinatsioonid. Džinni tarbitakse tavaliselt koos toonikuga (džinn toonikuga).",
@@ -28,12 +29,12 @@ function showInfo(drink) {
       "kokteilid": "Kokteil on kahest või enamast koostisosast koosnev jook. Kui algselt valmistati kokteile kiirelt ja kohapeal enne tarbimist, siis tänapäeval on levinud ka varem valmistatud ja hoidistatud kokteilid, mida tarvitatakse karastusjookidena. Tavaliselt liigitatakse kokteilid eelkõige alkohoolseteks ja alkoholivabadeks. Sageli lisatakse kokteilidele jääd, et suurendada nende värskendavat mõju, muuta maitset ning neid mõnikord ka lahjendada. Alkoholivabade kokteilide põhikomponendina pruugitakse sageli piima või jäätist, samuti mitmesuguseid mahlu. Kogu maailmas korraldatakse baarmenite vahel kokteilisegamises võistlusi, kus muuhulgas võisteldakse algupärastes kokteilides."
   };
 
-  // Get the corresponding image URL and text based on the drink type
+  // 
   const imageUrl = imageMap[drink] || 'pildid_lehel_2/default.jpg';
-  const description = textMap[drink] || "Teave selle joogi kohta pole saadaval.";
+  const description = textMap[drink] || "Teave selle joogi kohta pole saadaval."; */
 
   // Set the inner HTML of the infoDiv to include both the image and the text
-  infoDiv.innerHTML = `
+/*   infoDiv.innerHTML = `
   <div class="info-text">
       <h1>${drink.charAt(0).toUpperCase() + drink.slice(1)}</h1>
       <p>${description}</p>
@@ -42,6 +43,6 @@ function showInfo(drink) {
       <img src="${imageUrl}" alt="${drink}" width="300" height="400">
   </div>
 `;
-}
+} */
 
 /*Teinud ChatGPT*/
