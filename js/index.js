7972a913c31d0506dcc4720d956df52c3f42da06
Jesper Korsen <Jesper.Korsen@gmail.com>
2024-12-02 02:24:53 +0200

    otsingu js WIP

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// ------KAART------
 
 var kaart = L.map('kaart').setView([58.37939333946564, 26.723323522250354],17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -13,7 +14,40 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 for (const nimi of Object.keys(data.baarid)){
 
     console.log(data.baarid[nimi]);
-    const a = data.baarid[nimi]["pin"] = L.marker(data.baarid[nimi].asukoht).addTo(kaart)
-    a.bindPopup(nimi)
+    const a = data.baarid[nimi]["pin"] = L.marker(data.baarid[nimi].asukoht).addTo(kaart);
+    a.bindPopup(nimi);
 };
+
+// ------OTSING------
+
+const otsing = document.getElementById("otsing");
+
+let debounceTimer;
+const debounce = (callback, time) => {
+    window.clearTimeout(debounceTimer);
+    debounceTimer = window.setTimeout(callback, time);
+};
+
+otsing.addEventListener(
+    "input",
+    (event) => {
+        const query = event.target.value;
+        debounce(() => handleSeachPosts(query), 500);
+    },
+    false
+);
+
+const handleSearchPosts = (query) => {
+    const searchQuery = query.trim().toLowerCase();
+
+    if (searchQuery.length <= 1) {
+        return
+    }
+
+    let searchResults = [...data.baarid].filter(
+        (baar) =>
+            baar.
+
+    )
+}
 
